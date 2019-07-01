a7223c148af868b22747eec60170ab1f4e3c6b11
Kirilo Lozitsky <bondxxl01@gmail.com>
2019-07-01 23:32:06 +0300

    added jdbc groups authentication

--- web/resources/db/initDB.sql
+++ web/resources/db/initDB.sql
@@ -1,5 +1,10 @@
 DROP TABLE IF EXISTS authorities;
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS persistent_logins;
+DROP TABLE IF EXISTS group_authorities;
+DROP TABLE IF EXISTS group_members;
+DROP TABLE IF EXISTS `groups`;
+
 
 
 CREATE TABLE users
@@ -9,12 +14,6 @@ CREATE TABLE users
     enabled  BIT(1)      NOT NULL DEFAULT 1,
     primary key (username)
 );
-
-/*CREATE TABLE user_roles
-(
-    id INT(11) NOT NULL AUTO_INCREMENT,
-
-);*/
 
 
 CREATE TABLE authorities
@@ -31,6 +30,29 @@ CREATE TABLE persistent_logins
     username  VARCHAR(50) NOT NULL,
     series    VARCHAR(64) NOT NULL,
     token     VARCHAR(64) NOT NULL,
-    last_used TIMESTAMP NOT NULL,
+    last_used TIMESTAMP   NOT NULL,
     PRIMARY KEY (series)
+);
+
+CREATE TABLE `groups`
+(
+    id         BIGINT      NOT NULL AUTO_INCREMENT,
+    group_name VARCHAR(50) NOT NULL UNIQUE,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE group_authorities
+(
+    group_id  BIGINT      NOT NULL,
+    authority VARCHAR(50) NOT NULL,
+    FOREIGN KEY (group_id) REFERENCES `groups` (id)
+);
+
+CREATE TABLE group_members
+(
+    id       BIGINT      NOT NULL AUTO_INCREMENT,
+    username VARCHAR(50) NOT NULL,
+    group_id BIGINT      NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (group_id) REFERENCES `groups` (id)
 );
--- web/resources/db/populateDB.sql
+++ web/resources/db/populateDB.sql
@@ -12,3 +12,25 @@ VALUES ('admin', 'ROLE_ADMIN');
 
 INSERT INTO authorities(username, authority)
 VALUES ('admin', 'ROLE_USER');
+
+INSERT INTO `groups`(group_name)
+VALUES ('admins');
+
+INSERT INTO `groups`(group_name)
+VALUES ('users');
+
+INSERT INTO group_authorities(group_id, authority)
+VALUES (1, 'ROLE_ADMIN');
+
+INSERT INTO group_authorities(group_id, authority)
+VALUES (1, 'ROLE_USER');
+
+INSERT INTO group_authorities(group_id, authority)
+VALUES (2, 'ROLE_USER');
+
+INSERT INTO group_members(username, group_id)
+VALUES ('user', 2);
+
+INSERT INTO group_members(username, group_id)
+VALUES ('admin', 1);
+
